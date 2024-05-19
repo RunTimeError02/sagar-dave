f8e65bdf23d9206e777403a149e9f6c25147ab4b
RunTimeError02 <102181573+RunTimeError02@users.noreply.github.com>
2024-05-20 00:07:26 +0530

    ref bug not solved

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import React, { useRef, useState, useEffect } from "react";
@@ -6,14 +5,16 @@ import { asImageSrc, isFilled } from "@prismicio/client";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { MdArrowOutward } from "react-icons/md";
-import { Content } from '@prismicio/client';
+import { Content } from "@prismicio/client";
+
+gsap.registerPlugin(ScrollTrigger);
 
 type ContentListProps = {
-    items: Content.BlogPostDocument[] | Content.ProjectsDocument[];
-    contentType: Content.ContentIndexSlice["primary"]["content_type"];
-    fallbackItemImage: Content.ContentIndexSlice["primary"]["fallback_item_image"];
-    viewMoreText: Content.ContentIndexSlice["primary"]["view_more_text"];
-  };
+  items: Content.BlogPostDocument[] | Content.ProjectsDocument[];
+  contentType: Content.ContentIndexSlice["primary"]["content_type"];
+  fallbackItemImage: Content.ContentIndexSlice["primary"]["fallback_item_image"];
+  viewMoreText: Content.ContentIndexSlice["primary"]["view_more_text"];
+};
 
 export default function ContentList({
   items,
@@ -21,119 +22,118 @@ export default function ContentList({
   fallbackItemImage,
   viewMoreText = "Read More",
 }: ContentListProps) {
-    const component = useRef<HTMLUListElement>(null);
-    const itemsRef = useRef<Array<HTMLLIElement | null>>([]);
-  
-    const revealRef = useRef(null);
-    const [currentItem, setCurrentItem] = useState<null | number>(null);
-    const [hovering, setHovering] = useState(false);
-    const lastMousePos = useRef({ x: 0, y: 0 });
-  
-    const urlPrefix = contentType === "Blog" ? "/blog" : "/projects";
-    const ref = React.useRef<HTMLDivElement>(null);
+  const component = useRef(null);
+  const itemsRef = useRef<Array<HTMLLIElement | null>>([]);
 
-    useEffect(() => {
-      // Animate list-items in with a stagger
+  const revealRef = useRef(null);
+  const [currentItem, setCurrentItem] = useState<null | number>(null);
+  const [hovering, setHovering] = useState(false);
+  const lastMousePos = useRef({ x: 0, y: 0 });
+
+  const urlPrefix = contentType === "Blog" ? "/blog" : "/projects";
+
+  useEffect(() => {
+    // Animate list-items in with a stagger
+    let ctx = gsap.context(() => {
+      itemsRef.current.forEach((item, index) => {
+        gsap.fromTo(
+          item,
+          {
+            opacity: 0,
+            y: 20,
+          },
+          {
+            opacity: 1,
+            y: 0,
+            duration: 1.3,
+            ease: "elastic.out(1,0.3)",
+            stagger: 0.2,
+            scrollTrigger: {
+              trigger: item,
+              start: "top bottom-=100px",
+              end: "bottom center",
+              toggleActions: "play none none none",
+            },
+          },
+        );
+      });
+
+      return () => ctx.revert(); // cleanup!
+    }, component);
+  }, []);
+
+  useEffect(() => {
+    // Mouse move event listener
+    const handleMouseMove = (e: MouseEvent) => {
+      const mousePos = { x: e.clientX, y: e.clientY + window.scrollY };
+      // Calculate speed and direction
+      const speed = Math.sqrt(Math.pow(mousePos.x - lastMousePos.current.x, 2));
+
       let ctx = gsap.context(() => {
-        itemsRef.current.forEach((item, index) => {
-          gsap.fromTo(
-            item,
-            {
-              opacity: 0,
-              y: 20,
-            },
-            {
-              opacity: 1,
-              y: 0,
-              duration: 1.3,
-              ease: "elastic.out(1,0.3)",
-              stagger: 0.2,
-              scrollTrigger: {
-                trigger: item,
-                start: "top bottom-=100px",
-                end: "bottom center",
-                toggleActions: "play none none none",
-              },
-            },
-          );
-        });
-  
+        // Animate the image holder
+        if (currentItem !== null) {
+          const maxY = window.scrollY + window.innerHeight - 350;
+          const maxX = window.innerWidth - 250;
+
+          gsap.to(revealRef.current, {
+            x: gsap.utils.clamp(0, maxX, mousePos.x - 110),
+            y: gsap.utils.clamp(0, maxY, mousePos.y - 160),
+            rotation: speed * (mousePos.x > lastMousePos.current.x ? 1 : -1), // Apply rotation based on speed and direction
+            ease: "back.out(2)",
+            duration: 1.3,
+          });
+          gsap.to(revealRef.current, {
+            opacity: hovering ? 1 : 0,
+            visibility: "visible",
+            ease: "power3.out",
+            duration: 0.4,
+          });
+        }
+        lastMousePos.current = mousePos;
         return () => ctx.revert(); // cleanup!
       }, component);
-    }, []);
-  
-    useEffect(() => {
-      // Mouse move event listener
-      const handleMouseMove = (e: MouseEvent) => {
-        const mousePos = { x: e.clientX, y: e.clientY + window.scrollY };
-        // Calculate speed and direction
-        const speed = Math.sqrt(Math.pow(mousePos.x - lastMousePos.current.x, 2));
-  
-        let ctx = gsap.context(() => {
-          // Animate the image holder
-          if (currentItem !== null) {
-            const maxY = window.scrollY + window.innerHeight - 350;
-            const maxX = window.innerWidth - 250;
-  
-            gsap.to(revealRef.current, {
-              x: gsap.utils.clamp(0, maxX, mousePos.x - 110),
-              y: gsap.utils.clamp(0, maxY, mousePos.y - 160),
-              rotation: speed * (mousePos.x > lastMousePos.current.x ? 1 : -1), // Apply rotation based on speed and direction
-              ease: "back.out(2)",
-              duration: 1.3,
-            });
-            gsap.to(revealRef.current, {
-              opacity: hovering ? 1 : 0,
-              visibility: "visible",
-              ease: "power3.out",
-              duration: 0.4,
-            });
-          }
-          lastMousePos.current = mousePos;
-          return () => ctx.revert(); // cleanup!
-        }, component);
-      };
-  
-      window.addEventListener("mousemove", handleMouseMove);
-  
-      return () => {
-        window.removeEventListener("mousemove", handleMouseMove);
-      };
-    }, [hovering, currentItem]);
-  
-    const onMouseEnter = (index: number) => {
-      setCurrentItem(index);
-      if (!hovering) setHovering(true);
     };
-  
-    const onMouseLeave = () => {
-      setHovering(false);
-      setCurrentItem(null);
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
     };
-  
-    const contentImages = items.map((item) => {
-      const image = isFilled.image(item.data.hover_image)
-        ? item.data.hover_image
-        : fallbackItemImage;
-      return asImageSrc(image, {
-        fit: "crop",
-        w: 220,
-        h: 320,
-        exp: -10,
-      });
+  }, [hovering, currentItem]);
+
+  const onMouseEnter = (index: number) => {
+    setCurrentItem(index);
+    if (!hovering) setHovering(true);
+  };
+
+  const onMouseLeave = () => {
+    setHovering(false);
+    setCurrentItem(null);
+  };
+
+  const contentImages = items.map((item) => {
+    const image = isFilled.image(item.data.hover_image)
+      ? item.data.hover_image
+      : fallbackItemImage;
+    return asImageSrc(image, {
+      fit: "crop",
+      w: 220,
+      h: 320,
+      exp: -10,
     });
-  
-    // Preload images
-    useEffect(() => {
-      contentImages.forEach((url) => {
-        if (!url) return;
-        const img = new Image();
-        img.src = url;
-      });
-    }, [contentImages]);
+  });
+
+  // Preload images
+  useEffect(() => {
+    contentImages.forEach((url) => {
+      if (!url) return;
+      const img = new Image();
+      img.src = url;
+    });
+  }, [contentImages]);
+
   return (
     <>
-    
       <ul
         ref={component}
         className="grid border-b border-b-slate-100"
@@ -142,7 +142,7 @@ export default function ContentList({
         {items.map((post, index) => (
           <li
             key={index}
-            ref={(el) => (itemsRef.current[index] = el)}
+            // ref={(el) => (itemsRef.current[index] = el)}
             onMouseEnter={() => onMouseEnter(index)}
             className="list-item opacity-0"
           >
@@ -179,5 +179,5 @@ export default function ContentList({
         ></div>
       </ul>
     </>
-  )
+  );
 }

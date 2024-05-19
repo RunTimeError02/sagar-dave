9f29c4176900d8a91df6379cb87b4cb3c94681f5
RunTimeError02 <102181573+RunTimeError02@users.noreply.github.com>
2024-05-19 23:30:06 +0530

    ref bug

--- src/slices/ContentIndex/index.tsx
+++ src/slices/ContentIndex/index.tsx
@@ -24,20 +24,24 @@ const ContentIndex = async ({ slice }: ContentIndexProps): Promise<JSX.Element> 
 
   return (
     <Bounded
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      <Heading size="xl" className="mb-8">
-        {slice.primary.heading}
-      </Heading>
-      {isFilled.richText(slice.primary.description) && (
-        <div className="prose prose-xl prose-invert mb-10">
-          <PrismicRichText field={slice.primary.description} />
-        </div>
-      )}
-      <ContentList items = {items} contentType = {contentType} viewMoreText = {slice.primary.view_more_text}
-      fallbackItemImage={slice.primary.fallback_item_image}/>
-    </Bounded>
+    data-slice-type={slice.slice_type}
+    data-slice-variation={slice.variation}
+  >
+    <Heading size="xl" className="mb-8">
+      {slice.primary.heading}
+    </Heading>
+    {isFilled.richText(slice.primary.description) && (
+      <div className="prose prose-xl prose-invert mb-10">
+        <PrismicRichText field={slice.primary.description} />
+      </div>
+    )}
+    <ContentList
+      items={items}
+      contentType={slice.primary.content_type}
+      viewMoreText={slice.primary.view_more_text}
+      fallbackItemImage={slice.primary.fallback_item_image}
+    />
+  </Bounded>
   );
 };
 
